7370650b282055d128827fea7ee39dea65441c3b
Hugh Lyu <hugh.lv@gmail.com>
2025-02-22 17:03:30 -0800

    feat: card detail panel

--- Assets/Scripts/UI/Components/Card/CardDragHandler.cs
+++ Assets/Scripts/UI/Components/Card/CardDragHandler.cs
@@ -1,100 +1,105 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
 using System;
 using Kardx.UI.Scenes.Battle;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
 namespace Kardx.UI.Components.Card
 {
-  public class CardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
-  {
-    [Header("Drag Settings")]
-    [SerializeField] private float dragSpeed = 1f;
-    [SerializeField] private float returnSpeed = 10f;
-
-    private Vector3 originalPosition;
-    private bool isDragging;
-    private RectTransform rectTransform;
-    private Canvas canvas;
-
-    public event Action OnDragStarted;
-    public event Action<bool> OnDragEnded;
-
-    private void Awake()
+    public class CardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
-      rectTransform = GetComponent<RectTransform>();
-      canvas = GetComponentInParent<Canvas>();
-    }
+        [Header("Drag Settings")]
+        [SerializeField]
+        private float dragSpeed = 1f;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-      isDragging = true;
-      originalPosition = transform.position;
-      OnDragStarted?.Invoke();
-    }
+        [SerializeField]
+        private float returnSpeed = 10f;
 
-    public void OnDrag(PointerEventData eventData)
-    {
-      if (!isDragging) return;
+        private Vector3 originalPosition;
+        private bool isDragging;
+        private RectTransform rectTransform;
+        private Canvas canvas;
 
-      Vector2 position;
-      RectTransformUtility.ScreenPointToLocalPointInRectangle(
-        canvas.transform as RectTransform,
-        eventData.position,
-        canvas.worldCamera,
-        out position
-      );
+        public event Action OnDragStarted;
+        public event Action<bool> OnDragEnded;
 
-      transform.position = Vector3.Lerp(
-        transform.position,
-        canvas.transform.TransformPoint(position),
-        Time.deltaTime * dragSpeed
-      );
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-      isDragging = false;
-      bool wasSuccessful = false;
-
-      // Check if dropped on valid target
-      var raycastResults = new System.Collections.Generic.List<RaycastResult>();
-      EventSystem.current.RaycastAll(eventData, raycastResults);
-
-      foreach (var hit in raycastResults)
-      {
-        var dropZone = hit.gameObject.GetComponent<BattlefieldDropZone>();
-        if (dropZone != null && dropZone.IsValidDropTarget())
+        private void Awake()
         {
-          wasSuccessful = true;
-          break;
+            rectTransform = GetComponent<RectTransform>();
+            canvas = GetComponentInParent<Canvas>();
         }
-      }
 
-      if (!wasSuccessful)
-      {
-        ReturnToOriginalPosition();
-      }
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            isDragging = true;
+            originalPosition = transform.position;
+            OnDragStarted?.Invoke();
+        }
 
-      OnDragEnded?.Invoke(wasSuccessful);
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (!isDragging)
+                return;
+
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.transform as RectTransform,
+                eventData.position,
+                canvas.worldCamera,
+                out position
+            );
+
+            transform.position = Vector3.Lerp(
+                transform.position,
+                canvas.transform.TransformPoint(position),
+                Time.deltaTime * dragSpeed
+            );
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            isDragging = false;
+            bool wasSuccessful = false;
+
+            // Check if dropped on valid target
+            var raycastResults = new System.Collections.Generic.List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, raycastResults);
+
+            foreach (var hit in raycastResults)
+            {
+                var dropZone = hit.gameObject.GetComponent<BattlefieldDropZone>();
+                if (dropZone != null && dropZone.IsValidDropTarget())
+                {
+                    wasSuccessful = true;
+                    break;
+                }
+            }
+
+            if (!wasSuccessful)
+            {
+                ReturnToOriginalPosition();
+            }
+
+            OnDragEnded?.Invoke(wasSuccessful);
+        }
+
+        private void ReturnToOriginalPosition()
+        {
+            StartCoroutine(ReturnToPositionCoroutine());
+        }
+
+        private System.Collections.IEnumerator ReturnToPositionCoroutine()
+        {
+            float elapsedTime = 0;
+            Vector3 startPosition = transform.position;
+
+            while (elapsedTime < 1f)
+            {
+                elapsedTime += Time.deltaTime * returnSpeed;
+                transform.position = Vector3.Lerp(startPosition, originalPosition, elapsedTime);
+                yield return null;
+            }
+
+            transform.position = originalPosition;
+        }
     }
-
-    private void ReturnToOriginalPosition()
-    {
-      StartCoroutine(ReturnToPositionCoroutine());
-    }
-
-    private System.Collections.IEnumerator ReturnToPositionCoroutine()
-    {
-      float elapsedTime = 0;
-      Vector3 startPosition = transform.position;
-
-      while (elapsedTime < 1f)
-      {
-        elapsedTime += Time.deltaTime * returnSpeed;
-        transform.position = Vector3.Lerp(startPosition, originalPosition, elapsedTime);
-        yield return null;
-      }
-
-      transform.position = originalPosition;
-    }
-  }
 }
--- Assets/Scripts/UI/Components/Card/CardView.cs
+++ Assets/Scripts/UI/Components/Card/CardView.cs
@@ -1,211 +1,281 @@
 using System;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using Kardx.Core;
 using Kardx.Core.Data.Cards;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Kardx.UI.Components.Card
 {
-  using Card = Kardx.Core.Data.Cards.Card; // Alias for Card
-  public class CardView : MonoBehaviour
-  {
-    [Header("Card Data")]
-    [SerializeField] private Card card;
-    [SerializeField] private CardType cardType;
+    using Card = Kardx.Core.Data.Cards.Card; // Alias for Card
 
-    [Header("UI Elements")]
-    [SerializeField] private Image cardImage;
-    [SerializeField] private Image frameImage;
-    [SerializeField] private TextMeshProUGUI nameText;
-    [SerializeField] private TextMeshProUGUI descriptionText;
-    [SerializeField] private TextMeshProUGUI deploymentCostText;
-    [SerializeField] private TextMeshProUGUI operationCostText;
-    [SerializeField] private TextMeshProUGUI attackText;
-    [SerializeField] private TextMeshProUGUI defenceText;
-    [SerializeField] private TextMeshProUGUI abilityText;
-    [SerializeField] private TextMeshProUGUI abilityDescriptionText;
-    [SerializeField] private GameObject highlightEffect;
-
-    [Header("Animation")]
-    [SerializeField] private Animator animator;
-
-    private CardDragHandler dragHandler;
-    private bool isDraggable = true;
-
-    public Card Card => card;
-    public CardType CardType => cardType;
-
-    private void Awake()
+    public class CardView : MonoBehaviour
     {
-      dragHandler = GetComponent<CardDragHandler>();
-    }
+        [Header("Card Data")]
+        [SerializeField]
+        private Card card;
 
-    private void Start()
-    {
-      // UpdateCardView();
-    }
+        [SerializeField]
+        private CardType cardType;
 
-    public void UpdateCardView()
-    {
-      try
-      {
-        if (card == null && cardType == null)
+        [Header("UI Elements")]
+        [SerializeField]
+        private Image cardImage;
+
+        [SerializeField]
+        private Image frameImage;
+
+        [SerializeField]
+        private TextMeshProUGUI nameText;
+
+        [SerializeField]
+        private TextMeshProUGUI descriptionText;
+
+        [SerializeField]
+        private TextMeshProUGUI deploymentCostText;
+
+        [SerializeField]
+        private TextMeshProUGUI operationCostText;
+
+        [SerializeField]
+        private TextMeshProUGUI attackText;
+
+        [SerializeField]
+        private TextMeshProUGUI defenceText;
+
+        [SerializeField]
+        private TextMeshProUGUI abilityText;
+
+        [SerializeField]
+        private TextMeshProUGUI abilityDescriptionText;
+
+        [SerializeField]
+        private GameObject highlightEffect;
+
+        [Header("Components")]
+        [SerializeField]
+        private CardDragHandler dragHandler;
+
+        [Header("Animation")]
+        [SerializeField]
+        private Animator animator;
+
+        [Header("References")]
+        [SerializeField]
+        private CardDetailView cardDetailView;
+
+        private bool isDraggable = true;
+
+        public Card Card => card;
+        public CardType CardType => cardType;
+
+        private void Awake()
         {
-          Debug.LogWarning("[CardView] No card data available for update");
-          return;
+            dragHandler = GetComponent<CardDragHandler>();
         }
 
-        // Get the active card data source (either Card or CardType)
-        var name = card != null ? card.Name : cardType?.Name ?? "";
-        var description = card != null ? card.Description : cardType?.Description ?? "";
-        var deploymentCost = card != null ? card.DeploymentCost : cardType?.DeploymentCost ?? 0;
-        var operationCost = card != null ? card.OperationCost : cardType?.OperationCost ?? 0;
-        var attack = card != null ? card.Attack : cardType?.BaseAttack ?? 0;
-        var defence = card != null ? card.CurrentDefence : cardType?.BaseDefence ?? 0;
-        var imageUrl = card != null ? card.ImageUrl : cardType?.ImageUrl;
-        var abilities = card != null ? card.CardType.Abilities : cardType?.Abilities;
-
-        // Update UI elements safely
-        if (nameText != null) nameText.text = name;
-        if (descriptionText != null) descriptionText.text = description;
-        if (deploymentCostText != null) deploymentCostText.text = deploymentCost.ToString();
-        if (operationCostText != null) operationCostText.text = operationCost.ToString();
-        if (attackText != null) attackText.text = attack.ToString();
-        if (defenceText != null) defenceText.text = defence.ToString();
-
-        // Handle abilities if available
-        if (abilities != null && abilities.Count > 0 && abilities[0] != null)
+        private void Start()
         {
-          if (abilityText != null) abilityText.text = abilities[0].Name ?? "";
-          if (abilityDescriptionText != null) abilityDescriptionText.text = abilities[0].Description ?? "";
-        }
-        else
-        {
-          if (abilityText != null) abilityText.text = "";
-          if (abilityDescriptionText != null) abilityDescriptionText.text = "";
+            if (dragHandler == null)
+            {
+                dragHandler = GetComponent<CardDragHandler>();
+            }
         }
 
-        UpdateCardFrame();
-        if (card != null)
+        public void UpdateCardView()
         {
-          UpdateModifierEffects();
+            try
+            {
+                if (card == null && cardType == null)
+                {
+                    Debug.LogWarning("[CardView] No card data available for update");
+                    return;
+                }
+
+                // Get the active card data source (either Card or CardType)
+                var name = card != null ? card.Name : cardType?.Name ?? "";
+                var description = card != null ? card.Description : cardType?.Description ?? "";
+                var deploymentCost =
+                    card != null ? card.DeploymentCost : cardType?.DeploymentCost ?? 0;
+                var operationCost =
+                    card != null ? card.OperationCost : cardType?.OperationCost ?? 0;
+                var attack = card != null ? card.Attack : cardType?.BaseAttack ?? 0;
+                var defence = card != null ? card.CurrentDefence : cardType?.BaseDefence ?? 0;
+                var imageUrl = card != null ? card.ImageUrl : cardType?.ImageUrl;
+                var abilities = card != null ? card.CardType.Abilities : cardType?.Abilities;
+
+                // Update UI elements safely
+                if (nameText != null)
+                    nameText.text = name;
+                if (descriptionText != null)
+                    descriptionText.text = description;
+                if (deploymentCostText != null)
+                    deploymentCostText.text = deploymentCost.ToString();
+                if (operationCostText != null)
+                    operationCostText.text = operationCost.ToString();
+                if (attackText != null)
+                    attackText.text = attack.ToString();
+                if (defenceText != null)
+                    defenceText.text = defence.ToString();
+
+                // Handle abilities if available
+                if (abilities != null && abilities.Count > 0 && abilities[0] != null)
+                {
+                    if (abilityText != null)
+                        abilityText.text = abilities[0].Name ?? "";
+                    if (abilityDescriptionText != null)
+                        abilityDescriptionText.text = abilities[0].Description ?? "";
+                }
+                else
+                {
+                    if (abilityText != null)
+                        abilityText.text = "";
+                    if (abilityDescriptionText != null)
+                        abilityDescriptionText.text = "";
+                }
+
+                UpdateCardFrame();
+                if (card != null)
+                {
+                    UpdateModifierEffects();
+                }
+
+                LoadCardImage(imageUrl);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"[CardView] Error updating card view: {ex.Message}");
+            }
         }
 
-        LoadCardImage(imageUrl);
-      }
-      catch (Exception ex)
-      {
-        Debug.LogError($"[CardView] Error updating card view: {ex.Message}");
-      }
-    }
+        private void UpdateCardFrame()
+        {
+            var category =
+                card != null ? card.CardType.Category : cardType?.Category ?? CardCategory.Unit;
 
-    private void UpdateCardFrame()
-    {
-      var category = card != null ? card.CardType.Category : cardType?.Category ?? CardCategory.Unit;
-      
-      switch (category)
-      {
-        case CardCategory.Unit:
-          break;
-        case CardCategory.Order:
-          break;
-        case CardCategory.Countermeasure:
-          break;
-        case CardCategory.Headquarter:
-          break;
-      }
-    }
+            switch (category)
+            {
+                case CardCategory.Unit:
+                    break;
+                case CardCategory.Order:
+                    break;
+                case CardCategory.Countermeasure:
+                    break;
+                case CardCategory.Headquarter:
+                    break;
+            }
+        }
 
-    private void UpdateModifierEffects()
-    {
-      foreach (var modifier in card.Modifiers)
-      {
-      }
-    }
+        private void UpdateModifierEffects()
+        {
+            foreach (var modifier in card.Modifiers) { }
+        }
 
-    private void LoadCardImage(string url)
-    {
-      // Implement image loading logic here
-    }
+        private void LoadCardImage(string url)
+        {
+            // Implement image loading logic here
+        }
 
-    public void PlayDeployAnimation()
-    {
-      if (animator != null)
-      {
-        animator.SetTrigger("Deploy");
-      }
-    }
+        public void PlayDeployAnimation()
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Deploy");
+            }
+        }
 
-    public void PlayAttackAnimation()
-    {
-      if (animator != null)
-      {
-        animator.SetTrigger("Attack");
-      }
-    }
+        public void PlayAttackAnimation()
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Attack");
+            }
+        }
 
-    public void PlayDamageAnimation()
-    {
-      if (animator != null)
-      {
-        animator.SetTrigger("TakeDamage");
-      }
-    }
+        public void PlayDamageAnimation()
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("TakeDamage");
+            }
+        }
 
-    public void SetHighlight(bool isHighlighted)
-    {
-      if (highlightEffect != null)
-      {
-        highlightEffect.SetActive(isHighlighted);
-      }
-    }
+        public void SetHighlight(bool isHighlighted)
+        {
+            if (highlightEffect != null)
+            {
+                highlightEffect.SetActive(isHighlighted);
+            }
+        }
 
-    public void SetDraggable(bool canDrag)
-    {
-      isDraggable = canDrag;
-      if (dragHandler != null)
-      {
-        dragHandler.enabled = canDrag;
-      }
-    }
+        public void SetDraggable(bool canDrag)
+        {
+            isDraggable = canDrag;
+            if (dragHandler != null)
+            {
+                dragHandler.enabled = canDrag;
+            }
+        }
 
-    public void Initialize(Card card)
-    {
-      this.card = card;
-      this.cardType = null;
-      SetDraggable(true);
-      UpdateCardView();
-    }
+        public void Initialize(Card card)
+        {
+            this.card = card;
+            this.cardType = null;
+            SetDraggable(true);
+            UpdateCardView();
+        }
 
-    public void Initialize(CardType cardType)
-    {
-      this.card = null;
-      this.cardType = cardType;
-      SetDraggable(false); // CardType view should not be draggable
-      UpdateCardView();
-    }
+        public void Initialize(CardType cardType)
+        {
+            this.card = null;
+            this.cardType = cardType;
+            SetDraggable(false); // CardType view should not be draggable
+            UpdateCardView();
+        }
 
-    private void OnDestroy()
-    {
-      if (dragHandler != null)
-      {
-        dragHandler.OnDragStarted -= HandleDragStarted;
-        dragHandler.OnDragEnded -= HandleDragEnded;
-      }
-    }
+        public void ShowDetail()
+        {
+            Debug.Log("[CardView] Showing card detail view");
+            // Find CardDetailView even if inactive
+            cardDetailView = FindObjectOfType<CardDetailView>(true);
 
-    private void HandleDragStarted()
-    {
-      if (!isDraggable) return;
-      transform.SetAsLastSibling();
-    }
+            if (cardDetailView != null)
+            {
+                Debug.Log("[CardView] Found card detail view");
+                if (card != null)
+                {
+                    cardDetailView.Show(card);
+                }
+                else if (cardType != null)
+                {
+                    cardDetailView.Show(cardType);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("[CardView] No CardDetailView found in scene");
+            }
+        }
 
-    private void HandleDragEnded(bool wasSuccessful)
-    {
-      if (!wasSuccessful)
-      {
-      }
+        private void OnDestroy()
+        {
+            if (dragHandler != null)
+            {
+                dragHandler.OnDragStarted -= HandleDragStarted;
+                dragHandler.OnDragEnded -= HandleDragEnded;
+            }
+        }
+
+        private void HandleDragStarted()
+        {
+            if (!isDraggable)
+                return;
+            transform.SetAsLastSibling();
+        }
+
+        private void HandleDragEnded(bool wasSuccessful)
+        {
+            if (!wasSuccessful) { }
+        }
     }
-  }
 }
